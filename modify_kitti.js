06c494e8cfcc74bda19bcd125209af706ac61d82
Abhishek Anand <advenabhishek@gmail.com>
2022-01-13 11:41:15 +0530

    added fucntion to delete image with no label

--- modify_kitti.js
+++ modify_kitti.js
@@ -1,24 +1,40 @@
 const fs = require('fs')
 
-async function main(){
+async function correctKittiFormat() {
     let sampleDataRoot = '/home/abhian/Documents/samplekitti/'
     var filesArray = fs.readdirSync(sampleDataRoot).filter(file => fs.lstatSync(sampleDataRoot + file).isFile())
-    for(let file of filesArray){
-        console.log(file)
-        await processFile(sampleDataRoot+file)
+    for (let file of filesArray) {
+        await processFile(sampleDataRoot + file)
     }
 }
 
-async function processFile(file){
+async function processFile(file) {
     var contents = fs.readFileSync(file, 'utf8');
-    console.log(contents)
     contents = contents
-    .split('\n')
-    .map(row=>row.split(' ').slice(0, -1).join(' '))
-    .join('\n')
-    console.log(contents)
-    fs.writeFileSync(file,contents)
+        .split('\n')
+        .map(row => row.split(' ').slice(0, -1).join(' '))
+        .join('\n')
+    fs.writeFileSync(file, contents)
 }
 
 
-main()
+// correctKittiFormat()
+
+
+async function syncLabelAndTrainData() {
+    let count = 0
+    let trainFolder = '/data/tao_samples/tao-dataset/train/'
+    let labelFolder = '/data/tao_samples/tao-dataset/label/'
+    var filesArray = fs.readdirSync(sampleDataRoot).filter(file => fs.lstatSync(sampleDataRoot + file).isFile())
+    for (let file of filesArray) {
+        let labelFile = file.split('.')[0] + '.txt'
+        if (fs.existsSync(labelFolder + labelFile)) {
+            continue
+        } else {
+            // fs.unlink(trainFolder + file)
+            count++
+            console.log('delete file :: ', trainFolder + file)
+        }
+    }
+    console.log('Total files deleted :: ', count)
+}
